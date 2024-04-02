b71d6a68da02b6adb00983aba0d2c822a1f8686a
lukakurdadze <lukakurdadze2@gmail.com>
2024-04-02 11:23:38 +0400

    feat: add sorting to projects

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -18,6 +18,9 @@ class ProjectController extends Controller
     {
         $query = Project::query();
 
+        $sortField = request('sort_field', 'created_at');
+        $sortDirection = request('sort_direction', 'desc');
+
         if (request("name")) {
             $query->where("name", "like", "%", request("name") . "%");
         }
@@ -25,7 +28,7 @@ class ProjectController extends Controller
             $query->where("status", request("status"));
         }
 
-        $projects = $query->paginate(10)->onEachSide(1);
+        $projects = $query->orderBy($sortField, $sortDirection)->paginate(10)->onEachSide(1);
 
         return inertia("Project/Index", [
             "projects"    => ProjectResource::collection($projects),
